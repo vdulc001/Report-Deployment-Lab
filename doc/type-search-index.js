550f3714a353953bdd3054b83d5bd7543c9a2ab3
Vaelyn Dulce <vdulc001@odu.edu>
2021-11-12 00:57:40 -0500

    rm doc dir

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Author"},{"p":"<Unnamed>","l":"VocabTest"},{"p":"<Unnamed>","l":"Vocabulary"}];updateSearchResults();
